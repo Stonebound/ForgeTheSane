dc82862673a3bca1a10fe200234c9e781f4c15fe
phit <phit@hush.com>
2019-09-25 19:59:13 +0200

    multi file processing

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Turns the Forge Installer into a usable MultiMC instance')
     parser.add_argument(
-        '-i', '--input', help='path to forge-installer jar file', required=True)
+        '-i', '--input', nargs='+', type=argparse.FileType('r'), help='one or more paths to forge-installer jar files', required=True)
     parser.add_argument(
         '-f', '--force', help='force library redownload and instance recreation', action='store_true')
     args = parser.parse_args()
     # print(args)
     force = args.force
-    forge_name = args.input
-    lib_cache_setup.cache_setup()
-    procs = data_setup.data_setup(forge_name)
-    procedures.run_procs(procs)
-    multimc.make_instance(forge_name)
+
+    for file in args.input:
+        forge_file = file.name
+        # print(file.name)
+        lib_cache_setup.cache_setup()
+        procs = data_setup.data_setup(forge_file)
+        procedures.run_procs(procs)
+        multimc.make_instance(forge_file)
