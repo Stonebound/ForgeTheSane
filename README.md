aff110840a234ce34e0efc1b7f52a12439b4a5df
phit <phit@hush.com>
2020-03-21 18:29:42 +0100

    gui executable for windows

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # ForgeTheSane
 
-## Running
-`python3 main.py <path to forge installer>`
-Requires python 3.6 or later.
+## Windows
+
+Grab the latest release from the [Releases page](https://github.com/Stonebound/ForgeTheSane/releases)
+
+## Commandline
+
+```txt
+usage: main.py [-h] -i INPUT [INPUT ...] [-f]
+
+Turns the Forge Installer into a usable MultiMC instance
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT [INPUT ...], --input INPUT [INPUT ...]
+                        one or more paths to forge-installer jar files
+  -f, --force           force library redownload and instance recreation
+```
--- gui.py
+++ gui.py
@@ -1,13 +1,21 @@
+import data_setup
+import lib_cache_setup
+import procedures
+import multimc
+import sys
+import argparse
 from gooey import Gooey, GooeyParser
 
+force = False
 
-@Gooey(target="python3 main.py", program_name='Forge Downloader', suppress_gooey_flag=True)
+@Gooey()
 def main():
     parser = GooeyParser(description="Create Forge Instances for MultiMC")
     main = parser.add_argument_group('')
     main.add_argument('--input',
                       metavar='Forge Installer',
                       help='The Forge Installer Jar you want to process',
+                      nargs='+',
                       gooey_options={
                           'validator': {
                               'test': 'user_input[-4:] in {".jar"}',
@@ -15,15 +23,24 @@ def main():
                           }
                       },
                       required=True,
-                      widget='FileChooser')
+                      widget='MultiFileChooser')
     options = parser.add_argument_group('Options')  
     options.add_argument('--force',
                       metavar='Force Redownload & Recreation',
                       help='Will force the redownload of all libraries and recreation of the instance',
                       widget='CheckBox')
 
-    parser.parse_args()
+    args = parser.parse_args()
+    # print(args)
+    force = args.force
 
+    for file in args.input:
+        forge_file = file
+        # print(file.name)
+        lib_cache_setup.cache_setup()
+        procs = data_setup.data_setup(forge_file)
+        procedures.run_procs(procs)
+        multimc.make_instance(forge_file)
 
 if __name__ == '__main__':
     main()
